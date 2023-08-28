968e35a641f2c925c4b5b85a4fd44b8b398c9437
GPLer <GPLer@qq.com>
2023-08-28 19:57:20 +0800

    config: change docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,20 +6,21 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'GPLer\'s Website',
+  tagline: '🚧To Be Continued🚧',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://cxzx150133.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cxzx150133', // Usually your GitHub org/user name.
+  projectName: 'cxzx150133.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -64,7 +65,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'GPLer\'s Website',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
